45b9584eb27b8c23c5c7c4094f802f5187a639ea
heardofosodiumna <harrythegreat777@yahoo.com>
2017-09-01 11:14:41 -0400

    First Commit
    Project created in Unity, Pacman sprite added with animations.

---
+++ Pac-Man/Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanMove : MonoBehaviour {
+    public float speed = 0.4f
+	// Use this for initialization
+	void Start () {
+	    dest = transform.position
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+    bool valid(Vector2 dir) {
+        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+
+}
