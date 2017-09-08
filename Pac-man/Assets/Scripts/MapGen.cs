dd0eedecd9f08a4c3ceadb28654af72955ab93af
heardofosodiumna <harrythegreat777@yahoo.com>
2017-09-08 14:10:03 -0400

    Bug fixes and refactor of pellet pickup.
    Moved Pellet pickup script to each pellet instead of pacman.

---
+++ Pac-man/Assets/Scripts/MapGen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour {
+
+    public GameObject tl_soft_corner_border;
+    public GameObject tl_hard_corner_border;
+    public GameObject hor_straight_border;
+    public GameObject ver_straight_border;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
