40e2bc20da33c97e02b41187b92aae0026da5ab1
heardofosodiumna <harrythegreat777@yahoo.com>
2017-09-09 14:11:06 -0400

    Added walls and other sprites. Refactored pellet pickup.

--- Pac-Man/Assets/Scripts/PelletPickup.cs
+++ Pac-Man/Assets/Scripts/PelletPickup.cs
@@ -3,22 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PelletPickup : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Pellet")
+        if (other.gameObject.name == "pacman")
         {
-            Destroy(other.gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
--- Pac-man/Assets/Scripts/MapGen.cs
+++ Pac-man/Assets/Scripts/MapGen.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class MapGen : MonoBehaviour {
 
-    public GameObject tl_soft_corner_border;
-    public GameObject tl_hard_corner_border;
-    public GameObject hor_straight_border;
-    public GameObject ver_straight_border;
+    public GameObject tl_internal_corner;
+    public GameObject tr_internal_corner;
+    public GameObject tr_corner_internal;
+    public GameObject b_straight_internal;
+    public GameObject top_straight_border;
+    public GameObject bl_stub;
+    public GameObject br_stub;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 }
