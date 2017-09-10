29e77dccc5c2555460124f2537e3ad68e978c196
Rikusaber <huange2@rpi.edu>
2017-09-10 16:23:31 -0400

    fixed ghost script outside of house

---
+++ Pac-Man/Assets/Scripts/livesManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class livesManager : MonoBehaviour {
+    public float lives = 3.0f;
+    // Use this for initialization
+    void Start () {
+        lives = 3;
+        
+    }
+
+    public void death()
+    {
+        lives -= 1;
+        Debug.Log("You died. You have " + lives + " left!");
+        print("death");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
