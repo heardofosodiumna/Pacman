c8e89d35e6fb31f61d457c371083f427b80faba1
IgorCarvai <carvai@rpi.edu>
2017-09-24 15:17:42 -0400

    made jerry with rigidbody

--- hmwk2-2d/Assets/Scripts/playerMove.cs
+++ hmwk2-2d/Assets/Scripts/playerMove.cs
@@ -11,17 +11,21 @@ public class playerMove : MonoBehaviour {
     Vector2 closestPoint;
     float aDist;
     float shortestDist;
-    CharacterController controller;
+   // CharacterController controller;
     Quaternion turn;
     int targetIndex = 0;
+    public GameObject enemy;
+    float distEnemy;
+    Rigidbody2D rb;
 
-    bool control = true;
+    bool control = false;
     // Update is called once per frame
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+       // controller = GetComponent<CharacterController>();
         pathScript = pathScript.GetComponent<spawnCheese>();
+        rb = GetComponent<Rigidbody2D>();
         path = pathScript.points;
     }
     private void FixedUpdate () {
@@ -38,50 +42,74 @@ public class playerMove : MonoBehaviour {
             moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             moveDir = transform.TransformDirection(moveDir);
             moveDir = moveDir * speed;
-            controller.Move(moveDir * Time.deltaTime);
+           // controller.Move(moveDir * Time.deltaTime);
         }else
         { 
+            //path follow
             if (transform.rotation.z >= .5 || transform.rotation.z <= -.5)
                 gameObject.GetComponent<SpriteRenderer>().flipY = true;
             else
                 gameObject.GetComponent<SpriteRenderer>().flipY = false;
-
-            closestPoint = path[0];
-            shortestDist = Mathf.Sqrt(((closestPoint.x - transform.position.x) * (closestPoint.x - transform.position.x)) + ((closestPoint.y - transform.position.y) * (closestPoint.y - transform.position.y)));
-            foreach (Vector2 p in path)
+            distEnemy = Vector2.Distance(enemy.transform.position, transform.position);
+          //  Debug.Log("distEnemy: " + distEnemy);
+            if (distEnemy > 3.5)
             {
-               // Debug.Log("shortest is: " + shortestDist + " at point: " + closestPoint);
 
-                aDist = Mathf.Sqrt(((p.x - transform.position.x) * (p.x - transform.position.x)) + ((p.y - transform.position.y) * (p.y - transform.position.y)));
-               // Debug.Log("calculatiions is: " + aDist + " at point: " + p);
-
-                if (aDist <= shortestDist)
+                closestPoint = path[0];
+                shortestDist = Mathf.Sqrt(((closestPoint.x - transform.position.x) * (closestPoint.x - transform.position.x)) + ((closestPoint.y - transform.position.y) * (closestPoint.y - transform.position.y)));
+                foreach (Vector2 p in path)
                 {
-                    closestPoint = p;
-                    shortestDist = aDist;
-                }
-            }
-            // Debug.Log("closestPoint: " + closestPoint);
-            //closestPoint;
-            Vector2 target = path[targetIndex];
+                    // Debug.Log("shortest is: " + shortestDist + " at point: " + closestPoint);
 
-            //Rotate towards target
-            Vector2 vectorToTarget = target - (Vector2)transform.position;
-            float new_angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-            turn = Quaternion.AngleAxis(new_angle, Vector3.forward);
-         
-            //Move in current facing direction
-            transform.rotation = Quaternion.Slerp(transform.rotation, turn, Time.deltaTime * 3);
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
+                    aDist = Mathf.Sqrt(((p.x - transform.position.x) * (p.x - transform.position.x)) + ((p.y - transform.position.y) * (p.y - transform.position.y)));
+                    // Debug.Log("calculatiions is: " + aDist + " at point: " + p);
+
+                    if (aDist <= shortestDist)
+                    {
+                        closestPoint = p;
+                        shortestDist = aDist;
+                    }
+                }
+                // Debug.Log("closestPoint: " + closestPoint);
+                //closestPoint;
+                Vector2 target = path[targetIndex];
+
+                //Rotate towards target
+                Vector2 vectorToTarget = target - (Vector2)transform.position;
+                float new_angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+                turn = Quaternion.AngleAxis(new_angle, Vector3.forward);
+
+                //Move in current facing direction
+                transform.rotation = Quaternion.Slerp(transform.rotation, turn, Time.deltaTime * 3);
+                //rb.AddForce(transform.right * speed);
+                rb.velocity = (transform.right * speed);
+               
+                
+            }
+            else
+            {
+                //evade
+                Debug.Log(enemy.GetComponent<Rigidbody2D>().velocity);
+                /*
+                float t = distEnemy
+                // t is distance divied by max velocity
+                float t = distance / TargetAgent.MaxVelocity;
+                //point will be the Vec2 of target position + currentvel times t
+                Vector2 targetPoint = (Vector2)TargetAgent.transform.position + TargetAgent.CurrentVelocity * t;
+                //return the (target point - position).normlized * max veloxcity) - agent current velocity
+                return -(((targetPoint - (Vector2)transform.position).normalized * agent.MaxVelocity) - agent.CurrentVelocity);*/
+            }
 
         }
 
     }
-    void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter2D(Collider2D coll)
     {
-        Debug.Log("cleared: " + path[targetIndex]);
+        
+       // Debug.Log("cleared: " );
         path.Remove(path[targetIndex]);
-        Debug.Log("seeking: " + path[targetIndex]);
+      //  Debug.Log("seeking: " + path[targetIndex]);
     }
     /*
      *  public SteeringAgent TargetAgent;
