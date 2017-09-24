d78290a089b975e36ee9e1d70002ca309e6c27ae
IgorCarvai <carvai@rpi.edu>
2017-09-24 00:26:47 -0400

    cheese scripts update adn cheese prefab

--- hmwk2-2d/Assets/Scripts/spawnCheese.cs
+++ hmwk2-2d/Assets/Scripts/spawnCheese.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 public class spawnCheese : MonoBehaviour {
 
     
-    public Vector3 pos1 = new Vector3(-16, 1.5f ,14);
-    public Vector3 pos2 = new Vector3(9, 1.5f, 10);
-    public Vector3 pos3 = new Vector3(-5, 1.5f, -6);
     public GameObject cheese;
+    List<Vector2> points = new List<Vector2>();
+    Vector2 point = new Vector2(9f,-3f);
+    int howFar;
 
     void Start()
     { 
+        Vector2 pos2 = new Vector2(Random.Range(-9.5f, -7.5f), Random.Range(-3f, -2f));
+        Vector2 pos3 = new Vector2(Random.Range(7f, 8f), Random.Range(2f,3f));
+        Vector2 pos4 = new Vector2(Random.Range(-10f, 9f), Random.Range(3f, 4f));
 
-        Instantiate(cheese, pos1, Quaternion.identity);
-        Instantiate(cheese, Vector3.Lerp(pos1, pos2, 0.25f), Quaternion.identity);
-        Instantiate(cheese, Vector3.Lerp(pos1, pos2, 0.5f), Quaternion.identity);
-        Instantiate(cheese, Vector3.Lerp(pos1, pos2, 0.75f), Quaternion.identity);
-        Instantiate(cheese, pos2, Quaternion.identity);
-        Instantiate(cheese, Vector3.Lerp(pos2, pos3, 0.25f), Quaternion.identity);
-        Instantiate(cheese, Vector3.Lerp(pos2, pos3, 0.5f), Quaternion.identity);
-        Instantiate(cheese, Vector3.Lerp(pos2, pos3, 0.75f), Quaternion.identity);
-        Instantiate(cheese, pos3, Quaternion.identity);
+        Debug.Log("point2: " + pos2);
+        Instantiate(cheese, point, Quaternion.identity);
+        howFar = (int)Mathf.Sqrt(((pos2.x - point.x) * (pos2.x - point.x)) + ((pos2.y - point.y) * (pos2.x - point.x)));
+        Debug.Log(howFar);
+        for(int i=0; i < howFar; ++i)
+        {
+            point = Vector2.MoveTowards(point, pos2, 1);
+            point += new Vector2(0,Random.Range(0, -.4f));
+            points.Add(point);
+            Instantiate(cheese, point, Quaternion.identity);
+        }
+        howFar = (int)Mathf.Sqrt(((pos3.x - pos2.x) * (pos3.x - pos2.x)) + ((pos3.y - pos2.y) * (pos3.x - pos2.x)));
+        Debug.Log(howFar);
+        for (int i = 0; i < howFar; ++i)
+        {
+            point = Vector2.MoveTowards(point, pos3, 1);
+            point += new Vector2(0, Random.Range(-.4f, .4f));
+            points.Add(point);
+            Instantiate(cheese, point, Quaternion.identity);
+        }
+        howFar = (int)Mathf.Sqrt(((pos4.x - pos3.x) * (pos4.x - pos3.x)) + ((pos4.y - pos3.y) * (pos4.x - pos3.x)));
+        Debug.Log(howFar);
+        for (int i = 0; i < howFar; ++i)
+        {
+            point = Vector2.MoveTowards(point, pos4, 1);
+            point += new Vector2(0, Random.Range(0f, .4f));
+            points.Add(point);
+            Instantiate(cheese, point, Quaternion.identity);
+        }
+
+
     }
 
     // Update is called once per frame
