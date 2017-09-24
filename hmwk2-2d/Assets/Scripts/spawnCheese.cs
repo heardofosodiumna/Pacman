899a3074134df0ed38203fe858ea8904f870cf39
IgorCarvai <carvai@rpi.edu>
2017-09-24 01:01:43 -0400

    fixed small cheese bugs

--- hmwk2-2d/Assets/Scripts/spawnCheese.cs
+++ hmwk2-2d/Assets/Scripts/spawnCheese.cs
@@ -7,41 +7,55 @@ public class spawnCheese : MonoBehaviour {
     
     public GameObject cheese;
     List<Vector2> points = new List<Vector2>();
-    Vector2 point = new Vector2(9f,-3f);
+    Vector2 point = new Vector2(26f,-8f);
     int howFar;
+    public int div;
+    public int count;
 
     void Start()
     { 
-        Vector2 pos2 = new Vector2(Random.Range(-9.5f, -7.5f), Random.Range(-3f, -2f));
-        Vector2 pos3 = new Vector2(Random.Range(7f, 8f), Random.Range(2f,3f));
-        Vector2 pos4 = new Vector2(Random.Range(-10f, 9f), Random.Range(3f, 4f));
+        Vector2 pos2 = new Vector2(Random.Range(-32f, -28f), Random.Range(-9f, -7f));
+        Vector2 pos3 = new Vector2(Random.Range(24f, 28f), Random.Range(7f,9f));
+        Vector2 pos4 = new Vector2(Random.Range(-27f, -18f), Random.Range(6f, 9f));
 
         Debug.Log("point2: " + pos2);
         Instantiate(cheese, point, Quaternion.identity);
         howFar = (int)Mathf.Sqrt(((pos2.x - point.x) * (pos2.x - point.x)) + ((pos2.y - point.y) * (pos2.x - point.x)));
         Debug.Log(howFar);
-        for(int i=0; i < howFar; ++i)
+        int howMany = howFar / div;
+        for(int i=0; i < count; ++i)
         {
-            point = Vector2.MoveTowards(point, pos2, 1);
-            point += new Vector2(0,Random.Range(0, -.4f));
+            point = Vector2.MoveTowards(point, pos2, howMany);
+            point += new Vector2(0,Random.Range(0, -1f));
             points.Add(point);
             Instantiate(cheese, point, Quaternion.identity);
         }
+        
         howFar = (int)Mathf.Sqrt(((pos3.x - pos2.x) * (pos3.x - pos2.x)) + ((pos3.y - pos2.y) * (pos3.x - pos2.x)));
         Debug.Log(howFar);
-        for (int i = 0; i < howFar; ++i)
+        howMany = howFar / div;
+        for (int i = 0; i < count/2-1; ++i)
         {
-            point = Vector2.MoveTowards(point, pos3, 1);
-            point += new Vector2(0, Random.Range(-.4f, .4f));
+             point = Vector2.MoveTowards(point, pos3, howMany);
+             point += new Vector2(0, Random.Range(0f, 2f));
+             points.Add(point);
+             Instantiate(cheese, point, Quaternion.identity);
+        }
+        for (int i = 0; i < count / 2 - 1; ++i)
+        {
+            point = Vector2.MoveTowards(point, pos3, howMany);
+            point += new Vector2(0, Random.Range(-3f, 0f));
             points.Add(point);
             Instantiate(cheese, point, Quaternion.identity);
         }
+
         howFar = (int)Mathf.Sqrt(((pos4.x - pos3.x) * (pos4.x - pos3.x)) + ((pos4.y - pos3.y) * (pos4.x - pos3.x)));
         Debug.Log(howFar);
-        for (int i = 0; i < howFar; ++i)
+        howMany = howFar / div;
+        for (int i = 0; i < count; ++i)
         {
-            point = Vector2.MoveTowards(point, pos4, 1);
-            point += new Vector2(0, Random.Range(0f, .4f));
+            point = Vector2.MoveTowards(point, pos4, howMany);
+            point += new Vector2(0, Random.Range(0f, 1f));
             points.Add(point);
             Instantiate(cheese, point, Quaternion.identity);
         }
