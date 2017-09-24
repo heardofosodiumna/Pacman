f08dfb2a86308e17a61f1b787e49fee7aa25d607
Rikusaber <huange2@rpi.edu>
2017-09-24 19:14:52 -0400

    added ui text script

---
+++ hmwk2-2d/Assets/Scripts/jerrytext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class jerrytext : MonoBehaviour {
+    Text text;
+    public static string behavior = "";
+
+    // Use this for initialization
+    void Start () {
+        text = GetComponent<Text>();
+    }
+
+    public void setPathfinding()
+    {
+        behavior = "Pathfinding";
+    }
+
+    public void setEvade()
+    {
+        behavior = "Dynamic Evade";
+    }
+    // Update is called once per frame
+    void Update () {
+        text.text = "Jerry: " + behavior;
+    }
+}
