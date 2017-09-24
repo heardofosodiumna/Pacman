674bc9c851a753dd92a50ee04d42800dc802e0d8
IgorCarvai <carvai@rpi.edu>
2017-09-24 01:24:23 -0400

    commented on the evade script

--- hmwk2-2d/Assets/Scripts/playerMove.cs
+++ hmwk2-2d/Assets/Scripts/playerMove.cs
@@ -21,5 +21,31 @@ public class playerMove : MonoBehaviour {
         moveDir = transform.TransformDirection(moveDir);
         moveDir = moveDir * speed;
         controller.Move(moveDir * Time.deltaTime);
+
     }
+    /*
+     *  public SteeringAgent TargetAgent;
+        public float FleeRadius = 1.0f;
+        public bool DrawGizmos = false;
+
+        public override Vector2 GetVelocity()
+        {
+                //dist =   distact  from player to targer
+            float distance = Vector3.Distance(transform.position, TargetAgent.transform.position);
+
+            //if distance is <flee
+            if (distance < FleeRadius)
+            {
+                // t is distance divied by max velocity
+                float t = distance / TargetAgent.MaxVelocity;
+                //point will be the Vec2 of target position + currentvel times t
+                Vector2 targetPoint = (Vector2)TargetAgent.transform.position + TargetAgent.CurrentVelocity * t;
+                //return the (target point - position).normlized * max veloxcity) - agent current velocity
+                return -(((targetPoint - (Vector2)transform.position).normalized * agent.MaxVelocity) - agent.CurrentVelocity);
+            }
+            else
+                return Vector2.zero;
+        }
+
+    */
 }
