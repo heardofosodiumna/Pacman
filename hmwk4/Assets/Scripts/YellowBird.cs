053ebeb49a0d3d3555ceda25f332aeed3aafb5bd
IgorCarvai <carvai@rpi.edu>
2017-10-08 19:55:36 -0400

    initial commit for homework 4

---
+++ hmwk4/Assets/Scripts/YellowBird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBird : MonoBehaviour {
+    int next;
+    Vector3 nextPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int getNext()
+    {
+        return next;
+    }
+    public void incNext()
+    {
+        next++;
+    }
+    public void setPos(Vector3 p)
+    {
+        nextPos = p;
+    }
+    public Vector3 getPos()
+    {
+        return nextPos;
+    }
+}
