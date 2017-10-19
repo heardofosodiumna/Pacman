5c51d7ef952f48965f2df173b1d289a09b534fd6
Rikusaber <huange2@rpi.edu>
2017-10-19 19:11:27 -0400

    emergent

---
+++ hmwk4/Assets/Scripts/levelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Main");
+            print("1 pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Formation Test");
+            print("1 pressed");
+        }
+    }
+}
