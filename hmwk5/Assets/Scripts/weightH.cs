715dfc1a0d6089a5848bb60fd3c97a7e13bb8a0a
IgorCarvai <carvai@rpi.edu>
2017-11-06 15:53:57 -0500

    added button to change heuristic

---
+++ hmwk5/Assets/Scripts/weightH.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class weightH : MonoBehaviour {
+    public Slider hSlider;
+    public AlgorithmScript playerScript;
+    
+    void FixedUpdate()
+    {
+        GetComponentInChildren<Text>().text = "Weight of Heurisitc = "+ hSlider.value.ToString("0.0");
+    }
+    public void OnhSliderChanged()
+    {
+        playerScript.weightH = hSlider.value;
+    }
+}
